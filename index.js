c41cd2f610dbc98c20cbbd182a9f544726a35562
田盛絵美 <tamoriemi@tamorieminoMacBook-Pro.local>
2020-10-11 22:19:17 +0900

    git push heroku master

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-const line = require('@line/bot-sdk');
+const line = require('@line/bot-sdk');//@line/bot-sdk読み込み
+const { Client } = require('pg');//pgライブラリ読み込み
 const PORT = process.env.PORT || 5000
 
 const config = {
@@ -9,11 +10,21 @@ const config = {
 };
 
 const client = new line.Client(config);
+//Postgresを使うためのパラメータ初期設定
+const connection = new Client({
+    user:process.env.PG_USER,
+    host:process.env.PG_HOST,
+    database:process.env.PG_DATABASE,
+    password:process.env.PG_PASSWORD,
+    port:5432
+});
+connection.connect();//connectionを使ってデーターベース操作を行えるようになる
 
 app
 .post('/hook',line.middleware(config),(req,res)=> lineBot(req,res))
 .listen(PORT,()=>console.log(`Listening on ${PORT}`));
 
+//lineBot関数
 const lineBot = (req,res) => {
     res.status(200).end();
     const events = req.body.events;
@@ -35,6 +46,7 @@ Promise
 .catch(e=>console.error(e.stack));
 }
 
+//greeting_follow()
 const greeting_follow = async (ev) => {
     const profile = await client.getProfile(ev.source.userId);
     return client.replyMessage(ev.replyToken,{
@@ -42,7 +54,7 @@ const greeting_follow = async (ev) => {
         "text":`${profile.displayName}さん、フォローありがとうございます\uDBC0\uDC04`
     });
 }
-
+//handleMessageEvent()
 const handleMessageEvent = async (ev) => {
     const profile = await client.getProfile(ev.source.userId);
     const text = (ev.message.type === 'text') ? ev.message.text : '';
@@ -52,3 +64,14 @@ const handleMessageEvent = async (ev) => {
         "text":`${profile.displayName}さん、今${text}って言いました？`
     });
 }
+
+//顧客データーベースクエリ
+const create_userTable = {
+    text:'CREATE TABLE IF NOT EXISTS users (id SERIAL NOT NULL, line_uid VARCHAR(255), display_name VARCHAR(255), timestamp VARCHAR(255), cuttime SMALLINT, shampootime SMALLINT, colortime SMALLINT, spatime SMALLINT);'
+}
+//顧客データーベースクエリ実行
+connection.query(create_userTable)
+.then(()=>{
+    console.log('table users created successfully!!');
+})
+.catch(e=>console.log(e));
