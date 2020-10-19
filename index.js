cbeff3988ed35059f880846ee56636f8cd235f8b
田盛絵美 <tamoriemi@tamorieminoMacBook-Pro.local>
2020-10-19 17:22:00 +0900

    git push heroku master

--- index.js
+++ index.js
@@ -83,6 +83,9 @@ const handleMessageEvent = async (ev) => {
     if(text === '予約する'){
       const nextReservation = await checkNextReservation(ev);
       if(nextReservation.length){
+        const startTimestamp = nextReservation[0].starttime;
+        const date = dateConversion(startTimestamp);
+        const menu = MENU[parseInt(nextReservation[0].menu)];
         return client.replyMessage(ev.replyToken,{
           "type":"text",
           "text":`次回予約は${date}、${menu}でお取りしてます。変更の場合は予約キャンセル後改めて予約をお願いします。`
