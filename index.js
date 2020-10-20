e6a77a9838ba020001083b114bda627e583f7103
田盛絵美 <tamoriemi@tamorieminoMacBook-Pro.local>
2020-10-20 11:17:24 +0900

    git push heroku master

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ function getToday (){
   const month = today.getMonth() + 1;//月
   const day = today.getDate(); //日
   const present =  year + ',' + month + ',' + day;
+  return present;
   //console.log(present);
 }
 const config = {
@@ -703,8 +704,8 @@ const confirmation = (ev,menu,date,time) => {
     const selectedTime = 9 + parseInt(time);
     //現在の日付取得
     const today = getToday();
-    console.log("現在の日付：" + present.split(','));
-    console.log("予約日：" + splitDate.split(','));
+    console.log("現在の日付：" + today);
+    console.log("予約日：" + splitDate);
 
     return client.replyMessage(ev.replyToken,{
       "type":"flex",
