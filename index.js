3d06243c2d930be7c3c5d7d62902ac6ce84a5cab
田盛絵美 <tamoriemi@tamorieminoMacBook-Pro.local>
2020-10-23 20:24:01 +0900

    git push heroku master

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000
 const INITIAL_TREAT = [20,10,40,15,30,15,10];  //施術時間初期値
 const WEEK = [ "日", "月", "火", "水", "木", "金", "土" ];//曜日の表示を標準化
 const MENU = ['カット','シャンプー','カラーリング','ヘッドスパ','マッサージ＆スパ','眉整え','顔そり'];//メニュー名
-const HOLIDAY = ["月"];//定休日を設定
+const HOLIDAY = "月";//定休日を設定
 const config = {
     channelAccessToken:process.env.ACCESS_TOKEN,
     channelSecret:process.env.CHANNEL_SECRET
