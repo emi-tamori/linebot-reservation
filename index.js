94dd02b84c8b5e2faa35fde7527d3f39410d4004
田盛絵美 <tamoriemi@tamorieminoMacBook-Pro.local>
2020-11-03 13:58:49 +0900

    git push heroku master

--- index.js
+++ index.js
@@ -105,7 +105,7 @@ const handleMessageEvent = async (ev) => {
       if(nextReservation.length){
         const startTimestamp = nextReservation[0].starttime;
         const date = dateConversion(startTimestamp);
-        const menu = MENU[parseInt(nextReservation[0].menu)];
+        //const menu = MENU[parseInt(nextReservation[0].menu)];
         console.log("menu ="+ menu);
         console.log("nextReservation[0].menu = " + nextReservation[0].menu );
         const menuNumbers = nextReservation[0].menu;
