d5a0c8abc9ccd15a53cd5dd1b9ea5337a58a9a0b
田盛絵美 <tamoriemi@tamorieminoMacBook-Pro.local>
2020-11-09 22:23:40 +0900

    git push heroku master

--- index.js
+++ index.js
@@ -1073,6 +1073,8 @@ const checkAllReservation = (ev,treatTime) => {
         console.log('arr =',arr);
         const ts9 = new Date('2020/11/09 09:00').getTime();
         console.log('ts9 = '+ts9)
+        const ts92 = new Date(day +' 09:00').getTime();
+        console.log('ts92 = '+ts92)
 
       }else{
         resolve([]);
