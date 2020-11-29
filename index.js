5b5cce3668dd93a1beb5e317e4ef0b1b5afa006e
田盛絵美 <tamoriemi@tamorieminoMacBook-Pro.local>
2020-11-29 23:14:01 +0900

    git push heroku master

--- index.js
+++ index.js
@@ -226,7 +226,7 @@ const handlePostbackEvent = async (ev) => {
   const data = ev.postback.data;
   const splitData = data.split('&');
   const timeLimit = ev.postback.timeStamps;
-  const overTime = timeLimit + TIME_LIMIT;
+  const overTime = parseIn(timeLimit) + TIME_LIMIT;
 
   if(splitData[0] === 'menu'){
     console.log('timeLimit ='+timeLimit);
